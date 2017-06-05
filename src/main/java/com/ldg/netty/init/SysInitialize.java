875025ecf526825529e7a2a7cfba3522f898399a
LiuDongguang <liudongguang@qq.com>
2017-06-05 11:45:47 +0800

    Initial commit

---
+++ src/main/java/com/ldg/netty/init/SysInitialize.java
@@ -0,0 +1,24 @@
+package com.ldg.netty.init;
+
+import com.ldg.netty.websocket.chatServer.MyWebSocketChatServer;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.servlet.ServletContext;
+
+/**
+ * Created by LiuDongguang on 2017/6/5.
+ */
+public class SysInitialize implements ServletContextAware {
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        Thread thread=new Thread(() -> {
+            try {
+                MyWebSocketChatServer.startWebSocket();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+}
